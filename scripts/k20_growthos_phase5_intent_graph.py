6c4facc54e27180f11a9dcf95a8280685a81ce35
calibra-dev <keshavarz20.com@gmail.com>
2026-09-24 10:41:27 +0330

    growthos phase5: repair owner-map syntax

--- scripts/k20_growthos_phase5_intent_graph.py
+++ scripts/k20_growthos_phase5_intent_graph.py
@@ -22,8 +22,6 @@ OWNER_HINTS = {
     'لوله مه پاش': 'https://keshavarz20.com/product/%D9%84%D9%88%D9%84%D9%87-%D8%A8%D8%A7%D8%B1%D8%A7%D9%86%DB%8C-%D9%85%D9%87-%D9%BE%D8%A7%D8%B4-%D8%A2%D8%B3%D8%A7%DB%8C%D8%B4-%D8%A2%D8%B0%D8%B1%D8%A8%D8%A7%DB%8C%D8%AC%D8%A7%D9%86-%DB%B1%DB%B0%DB%B0/'
 }
 
-def fa_norm
-
 def fa_norm(s):
     s = (s or '').strip().lower()
     s = s.replace('ي','ی').replace('ك','ک').replace('\u200c',' ')
